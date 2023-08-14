b589d16a2aea1fec8991ab0cd749f402eb7824ae
andres.mancheno <andres.mancheno@finetwork.com>
2023-08-14 06:44:04 +0200

    Structure created

---
+++ packages/@finetwork:ui/src/components/NewAccordion/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+import { AccordionContainer } from './styled'
+import { AccordionProps } from './types'
+
+export const NewAccordion: FC<AccordionProps> = () => {
+  return <AccordionContainer>d</AccordionContainer>
+}
